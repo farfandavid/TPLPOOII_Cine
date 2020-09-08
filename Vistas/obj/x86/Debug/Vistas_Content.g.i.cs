e67489e75d8805efd075e1ef47c072183bd43c66
DavidDF <38300010+shizukaddf@users.noreply.github.com>
2020-09-08 20:29:10 -0300

    ChangeImage

--- Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
+++ Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/mainimage.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/mainimage2.png")]
 
 
